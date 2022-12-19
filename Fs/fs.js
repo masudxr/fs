665360a41b1cd223f3b0cd6b60e92e2b7f2979c7
masudxr <masudxr@example.com>
2022-12-19 13:32:14 +0600

    file system updated

---
+++ Fs/fs.js
@@ -0,0 +1,40 @@
+import fs from 'fs';
+
+// Write Array of object in the CSV file//
+
+let data = [];
+class person {
+    constructor(id, name, age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
+// data.push(new person('id', 'Masud', 'age'));
+data.push(new person(1, 'Masud', 2229));
+data.push(new person(2, 'Rana', 2329));
+data.push(new person(3, 'Shihab', 129));
+data.push(new person(4, 'Mridha', 290));
+data.push(new person(5, 'Methu', 200));
+data.push(new person(6, 'Masud', 2229));
+
+const filewrite = (file, content) => {
+    return new Promise((resolve, reject) => {
+        fs.appendFile(file, content, (err) => {
+            if (err) reject(err)
+            if(!err) resolve('Append Success')
+        })
+    })
+};
+async function writecall(data) {
+    // console.log('data:', data)
+    for (let i = 0; i < data.length; i++) {
+        // console.log('data Length:', data.length)
+        let content = data[i].id + ',' + data[i].name + ',' + data[i].age + '\n';
+        // console.log('Content:', content)
+        await filewrite('mov2.csv', content);
+    }
+}
+writecall(data).catch((err) => {
+    console.log('error:', err)
+})
